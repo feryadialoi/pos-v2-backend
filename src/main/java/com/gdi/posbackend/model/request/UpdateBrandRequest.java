ec6cb30008d8d15462d07b0875a19663eb966f9b
Feryadi Aloi <feryadialoi@gmail.com>
2021-08-13 23:36:10 +0700

    create model BrandResponse

---
+++ src/main/java/com/gdi/posbackend/model/request/UpdateBrandRequest.java
@@ -0,0 +1,12 @@
+package com.gdi.posbackend.model.request;
+
+import lombok.Data;
+
+/**
+ * @author Feryadialoi
+ * @date 8/13/2021 3:09 PM
+ */
+@Data
+public class UpdateBrandRequest {
+    private String name;
+}
