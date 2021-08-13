f8d1bbfb36ec17946201ab1c8222b1a7761ceea8
Feryadi Aloi <feryadialoi@gmail.com>
2021-08-13 23:22:42 +0700

    setup audit for entity

---
+++ src/main/java/com/gdi/posbackend/audit/AuditTrailListener.java
@@ -0,0 +1,37 @@
+package com.gdi.posbackend.audit;
+
+import com.gdi.posbackend.entity.auth.User;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.persistence.*;
+
+/**
+ * @author Feryadialoi
+ * @date 7/24/2021 10:10 AM
+ */
+@Slf4j
+public class AuditTrailListener {
+
+    @PrePersist
+    @PreUpdate
+    @PreRemove
+    private void beforeAnyUpdate(User user) {
+        if (user.getId() == 0) {
+            log.info("[USER AUDIT] About to add a user");
+        } else {
+            log.info("[USER AUDIT] About to update/delete user: " + user.getId());
+        }
+    }
+
+    @PostPersist
+    @PostUpdate
+    @PostRemove
+    private void afterAnyUpdate(User user) {
+        log.info("[USER AUDIT] add/update/delete complete for user: " + user.getId());
+    }
+
+    @PostLoad
+    private void afterLoad(User user) {
+        log.info("[USER AUDIT] user loaded from database: " + user.getId());
+    }
+}
---
+++ src/main/java/com/gdi/posbackend/audit/AuditorProvider.java
@@ -0,0 +1,35 @@
+package com.gdi.posbackend.audit;
+
+import com.gdi.posbackend.security.ApplicationUserDetails;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+/**
+ * @author Feryadialoi
+ * @date 8/5/2021 4:14 AM
+ */
+@Component("auditorProvider")
+public class AuditorProvider implements AuditorAware<Long> {
+
+    @Override
+    public Optional<Long> getCurrentAuditor() {
+        if (SecurityContextHolder.getContext() != null && SecurityContextHolder.getContext().getAuthentication() != null) {
+            if (SecurityContextHolder.getContext().getAuthentication().getPrincipal() != "anonymousUser") {
+                return Optional.ofNullable(SecurityContextHolder.getContext())
+                        .map(SecurityContext::getAuthentication)
+                        .filter(Authentication::isAuthenticated)
+                        .map(Authentication::getPrincipal)
+                        .map(ApplicationUserDetails.class::cast)
+                        .map(ApplicationUserDetails::getId);
+            }
+
+            return Optional.empty();
+        }
+        return Optional.empty();
+    }
+}
