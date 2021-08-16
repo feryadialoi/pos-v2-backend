e05f0871693c18fd81d92c3a2e3e02e3f890998f
Feryadi Aloi <feryadialoi@gmail.com>
2021-08-16 23:47:36 +0700

    add some props to ProductStockServiceImpl

--- src/main/java/com/gdi/posbackend/service/impl/ProductStockServiceImpl.java
+++ src/main/java/com/gdi/posbackend/service/impl/ProductStockServiceImpl.java
@@ -1,7 +1,9 @@
 package com.gdi.posbackend.service.impl;
 
+import com.gdi.posbackend.entity.Product;
 import com.gdi.posbackend.model.criteria.ProductStockCriteria;
 import com.gdi.posbackend.model.request.UpdateProductStockRequest;
+import com.gdi.posbackend.repository.ProductRepository;
 import com.gdi.posbackend.service.ProductStockService;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -15,8 +17,16 @@ import org.springframework.stereotype.Service;
 @Service
 @AllArgsConstructor
 public class ProductStockServiceImpl implements ProductStockService {
+
+    private final ProductRepository productRepository;
+
+
     @Override
     public Page<Object> getProductStocks(ProductStockCriteria productStockCriteria, Pageable pageable) {
+
+        Product product = new Product();
+        productRepository.findAll(pageable);
+
         return null;
     }
 
