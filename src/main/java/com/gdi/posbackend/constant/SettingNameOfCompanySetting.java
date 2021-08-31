56cc1807e8dd68b170d9edf4dbd736ae66b036a9
Feryadi Aloi <feryadialoi@gmail.com>
2021-08-31 13:33:34 +0700

    create class for constant value of SettingNameOfCompanySetting

---
+++ src/main/java/com/gdi/posbackend/constant/SettingNameOfCompanySetting.java
@@ -0,0 +1,45 @@
+package com.gdi.posbackend.constant;
+
+/**
+ * @author Feryadialoi
+ * @date 8/28/2021 3:20 AM
+ */
+public enum SettingNameOfCompanySetting {
+
+    UNIT,
+    ACCOUNTING,
+    COGS,
+    IS_TAXED,
+    TAX_RATED,
+
+    COA_PURCHASE,
+    COA_SALE,
+    COA_INCOME_SUMMARY,
+    COA_RETAINED_EARNING,
+    COA_PAYABLE,
+    COA_RECEIVABLE,
+    COA_TAX_IN,
+    COA_TAX_OUT,
+    COA_DISCOUNT_IN,
+    COA_DISCOUNT_OUT,
+
+    COGS_METHOD,
+
+//    public static final String UNIT = "UNIT";
+//    public static final String ACCOUNTING = "ACCOUNTING";
+//    public static final String COGS = "COGS";
+//    public static final String IS_TAXED = "IS_TAXED";
+//    public static final String TAX_RATED = "TAX_RATED";
+//
+//    public static final String COA_PURCHASE = "COA_PURCHASE";
+//    public static final String COA_SALE = "COA_SALE";
+//    public static final String COA_INCOME_SUMMARY = "COA_INCOME_SUMMARY";
+//    public static final String COA_RETAINED_EARNING = "COA_RETAINED_EARNING";
+//    public static final String COA_PAYABLE = "COA_PAYABLE";
+//    public static final String COA_RECEIVABLE = "COA_RECEIVABLE";
+//    public static final String COA_TAX_IN = "COA_TAX_IN";
+//    public static final String COA_TAX_OUT = "COA_TAX_OUT";
+//    public static final String COA_DISCOUNT_IN = "COA_DISCOUNT_IN";
+//    public static final String COA_DISCOUNT_OUT = "COA_DISCOUNT_OUT";
+
+}
