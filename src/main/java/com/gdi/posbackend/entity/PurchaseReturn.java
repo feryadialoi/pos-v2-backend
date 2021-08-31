679c0be111bfbe26aa52e25eb2ad60b5c7359804
Feryadi Aloi <feryadialoi@gmail.com>
2021-08-31 13:26:38 +0700

    create new entity of PurchaseReturn and SaleReturn

---
+++ src/main/java/com/gdi/posbackend/entity/PurchaseReturn.java
@@ -0,0 +1,8 @@
+package com.gdi.posbackend.entity;
+
+/**
+ * @author Feryadialoi
+ * @date 8/29/2021 3:50 AM
+ */
+public class PurchaseReturn {
+}
---
+++ src/main/java/com/gdi/posbackend/entity/SaleReturn.java
@@ -0,0 +1,11 @@
+package com.gdi.posbackend.entity;
+
+import lombok.NoArgsConstructor;
+
+/**
+ * @author Feryadialoi
+ * @date 8/29/2021 3:54 AM
+ */
+@NoArgsConstructor
+public class SaleReturn {
+}
