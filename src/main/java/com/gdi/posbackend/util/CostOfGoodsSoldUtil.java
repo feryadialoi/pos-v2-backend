87abcf26b50f2bbd0475235b07ab070afdfa380d
Feryadi Aloi <feryadialoi@gmail.com>
2021-08-31 11:52:55 +0700

    create utility class of CostOfGoodsSoldUtil

---
+++ src/main/java/com/gdi/posbackend/util/CostOfGoodsSoldUtil.java
@@ -0,0 +1,18 @@
+package com.gdi.posbackend.util;
+
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+/**
+ * @author Feryadialoi
+ * @date 8/20/2021 3:16 AM
+ */
+@Component
+public class CostOfGoodsSoldUtil {
+
+    BigDecimal calculate() {
+        return BigDecimal.ZERO;
+    }
+
+}
