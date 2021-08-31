e7f4f08348f69fe1245ad16fa14510811492a97d
Feryadi Aloi <feryadialoi@gmail.com>
2021-08-31 13:23:34 +0700

    create new entity of Journal and JournalDetail

---
+++ src/main/java/com/gdi/posbackend/entity/Journal.java
@@ -0,0 +1,47 @@
+package com.gdi.posbackend.entity;
+
+import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * @author Feryadialoi
+ * @date 8/20/2021 3:42 AM
+ */
+@Getter
+@Setter
+@ToString(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "journals")
+@SQLDelete(sql = "UPDATE journals SET deleted_date = NOW() WHERE id = ?")
+@Where(clause = "deleted_date is null")
+public class Journal extends BaseEntity {
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "entry_date")
+    private LocalDateTime entryDate;
+
+    @Column(name = "debit")
+    private BigDecimal debit;
+
+    @Column(name = "credit")
+    private BigDecimal credit;
+
+    @Column(name = "description")
+    private String description;
+
+    @OneToMany(mappedBy = "journal", cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH})
+    private List<JournalDetail> journalDetails;
+}
---
+++ src/main/java/com/gdi/posbackend/entity/JournalDetail.java
@@ -0,0 +1,45 @@
+package com.gdi.posbackend.entity;
+
+import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+/**
+ * @author Feryadialoi
+ * @date 8/20/2021 3:42 AM
+ */
+@Getter
+@Setter
+@ToString(callSuper = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "journal_details")
+@SQLDelete(sql = "UPDATE journal_details SET deleted_date = NOW() WHERE id = ?")
+@Where(clause = "deleted_date is null")
+public class JournalDetail extends BaseEntity {
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH})
+    @JoinColumn(name = "journal_id", referencedColumnName = "id")
+    private Journal journal;
+
+    @OneToOne
+    @JoinColumn(name = "chart_of_account_id", referencedColumnName = "id")
+    private ChartOfAccount chartOfAccount;
+
+    @Column(name = "entry_date")
+    private LocalDateTime entryDate;
+
+    @Column(name = "debit")
+    private BigDecimal debit;
+
+    @Column(name = "credit")
+    private BigDecimal credit;
+
+    @Column(name = "description")
+    private String description;
+}
