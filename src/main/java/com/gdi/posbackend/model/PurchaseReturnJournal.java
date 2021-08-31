1a5c4e0318db05ffcd2723b5f9779eccd1d6fe96
Feryadi Aloi <feryadialoi@gmail.com>
2021-08-31 13:48:07 +0700

    create general use model

---
+++ src/main/java/com/gdi/posbackend/model/PurchaseReturnJournal.java
@@ -0,0 +1,24 @@
+package com.gdi.posbackend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+/**
+ * @author Feryadialoi
+ * @date 8/25/2021 1:25 PM
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PurchaseReturnJournal {
+    private BigDecimal amount;
+    private BigDecimal tax;
+    private BigDecimal discount;
+    private BigDecimal shippingFee;
+    private BigDecimal otherFee;
+}
---
+++ src/main/java/com/gdi/posbackend/model/StockCard.java
@@ -0,0 +1,15 @@
+package com.gdi.posbackend.model;
+
+/**
+ * @author Feryadialoi
+ * @date 8/20/2021 3:18 AM
+ */
+public class StockCard {
+    /**
+     * kartu stok / persediaan barang dagang
+     * sistem perpetual(terus-menerus)
+     * LIFO
+     * FIFO
+     * Weighted Average
+     */
+}
