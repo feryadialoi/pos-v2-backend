9bbdb61d610269f8d218417392cc0a93e7e0be14
Feryadi Aloi <feryadialoi@gmail.com>
2021-08-31 13:52:05 +0700

    create controller of CompanyController and CompanyControllerAdvice

---
+++ src/main/java/com/gdi/posbackend/controller/controlleradvice/CompanyControllerAdvice.java
@@ -0,0 +1,19 @@
+package com.gdi.posbackend.controller.controlleradvice;
+
+import com.gdi.posbackend.controller.BaseControllerAdvice;
+import com.gdi.posbackend.exception.CompanyNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * @author Feryadialoi
+ * @date 8/30/2021 11:12 AM
+ */
+@RestControllerAdvice
+public class CompanyControllerAdvice extends BaseControllerAdvice {
+    @ExceptionHandler(CompanyNotFoundException.class)
+    public Object companyNotFound(CompanyNotFoundException companyNotFoundException) {
+        return response("not found", companyNotFoundException.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
---
+++ src/main/java/com/gdi/posbackend/controller/v1/CompanyController.java
@@ -0,0 +1,54 @@
+package com.gdi.posbackend.controller.v1;
+
+import com.gdi.posbackend.controller.BaseController;
+import com.gdi.posbackend.model.criteria.CompanyCriteria;
+import com.gdi.posbackend.model.request.CreateCompanyRequest;
+import com.gdi.posbackend.model.request.UpdateCompanyRequest;
+import com.gdi.posbackend.model.response.ApiResponse;
+import com.gdi.posbackend.model.response.CompanyResponse;
+import com.gdi.posbackend.service.CompanyService;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+/**
+ * @author Feryadialoi
+ * @date 8/29/2021 4:05 PM
+ */
+@RestController
+@RequestMapping("/api/v1/companies")
+@AllArgsConstructor
+public class CompanyController extends BaseController {
+
+    // ** service
+    private final CompanyService companyService;
+
+    // TODO get company restrict to super admin only
+    @GetMapping
+    public ResponseEntity<ApiResponse<Page<CompanyResponse>>> getCompanies(CompanyCriteria companyCriteria, Pageable pageable) {
+        return response("get companies success", companyService.getCompanies(companyCriteria, pageable));
+    }
+
+    // TODO get company restrict to related company only
+    @GetMapping("/{companyId}")
+    public ResponseEntity<ApiResponse<CompanyResponse>> getCompany(@PathVariable(name = "companyId") String companyId) {
+        return response("get company success", companyService.getCompany(companyId));
+    }
+
+    // TODO create company restrict to super admin only
+    @PostMapping
+    public ResponseEntity<ApiResponse<CompanyResponse>> createCompany(@Valid @RequestBody CreateCompanyRequest createCompanyRequest) {
+        return response("create company success", companyService.createCompany(createCompanyRequest));
+    }
+
+    // TODO update company restrict to related company only
+    @PutMapping("/{companyId}")
+    public ResponseEntity<ApiResponse<Object>> updateCompany(@PathVariable(name = "companyId") String companyId, @Valid @RequestBody UpdateCompanyRequest updateCompanyRequest) {
+        return response("update company success", null);
+    }
+
+}
