41481574a4f31d70f1355cc373aa36dba4f4581e
Feryadi Aloi <feryadialoi@gmail.com>
2021-08-31 13:24:21 +0700

    create new entity of Purchase and PurchaseDetail

---
+++ src/main/java/com/gdi/posbackend/entity/Purchase.java
@@ -0,0 +1,97 @@
+package com.gdi.posbackend.entity;
+
+import com.gdi.posbackend.entity.enums.PurchaseStatus;
+import com.gdi.posbackend.entity.enums.PaymentType;
+import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * @author Feryadialoi
+ * @date 8/25/2021 1:53 AM
+ */
+@ToString
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "purchases")
+@SQLDelete(sql = "UPDATE purchases SET deleted_date = NOW() WHERE id = ?")
+@Where(clause = "deleted_date is null")
+public class Purchase extends BaseEntity {
+
+    @OneToOne
+    @ToString.Exclude
+    private PurchaseOrder purchaseOrder;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "reference")
+    private String reference;
+
+    @OneToOne
+    @JoinColumn(name = "supplier_id", referencedColumnName = "id")
+    private Supplier supplier;
+
+    @Column(name = "is_taxed")
+    private Boolean isTaxed;
+
+    @Column(name = "tax")
+    private BigDecimal tax;
+
+    @Column(name = "is_discounted")
+    private Boolean isDiscounted;
+
+    @Column(name = "discount")
+    private BigDecimal discount;
+
+    @Column(name = "entry_date")
+    private LocalDateTime entryDate;
+
+    @Column(name = "due_date")
+    private LocalDateTime dueDate;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "payment_type", columnDefinition = "enum('CASH','CREDIT')")
+    private PaymentType paymentType;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", columnDefinition = "enum('DRAFT','APPROVED', 'AWAITING_APPROVAL')")
+    private PurchaseStatus status;
+
+    @Column(name = "total")
+    private BigDecimal total;
+
+    @Column(name = "grand_total")
+    private BigDecimal grandTotal;
+
+    @Column(name = "note")
+    private String note;
+
+    @Column(name = "attachment")
+    private String attachment;
+
+    @Column(name = "shipping_fee")
+    private BigDecimal shippingFee;
+
+    @Column(name = "shipping_fee_description")
+    private String shippingFeeDescription;
+
+    @Column(name = "other_fee")
+    private BigDecimal otherFee;
+
+    @Column(name = "other_fee_description")
+    private String otherFeeDescription;
+
+    @OneToMany(mappedBy = "purchase", cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH})
+    @ToString.Exclude
+    private List<PurchaseDetail> purchaseDetails;
+
+}
---
+++ src/main/java/com/gdi/posbackend/entity/PurchaseDetail.java
@@ -0,0 +1,65 @@
+package com.gdi.posbackend.entity;
+
+import com.gdi.posbackend.entity.enums.DiscountFormat;
+import com.gdi.posbackend.entity.enums.TaxFormat;
+import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+/**
+ * @author Feryadialoi
+ * @date 8/25/2021 1:54 AM
+ */
+@ToString
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "purchase_details")
+@SQLDelete(sql = "UPDATE purchase_details SET deleted_date = NOW() WHERE id = ?")
+@Where(clause = "deleted_date is null")
+public class PurchaseDetail extends BaseEntity {
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH})
+    @JoinColumn(name = "purchase_id", referencedColumnName = "id")
+    private Purchase purchase;
+
+    @OneToOne
+    @JoinColumn(name = "product_id", referencedColumnName = "id")
+    private Product product;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+    @Column(name = "quantity")
+    private BigDecimal quantity;
+
+    @OneToOne
+    @JoinColumn(name = "unit_id", referencedColumnName = "id")
+    private Unit unit;
+
+    @OneToOne
+    @JoinColumn(name = "warehouse_id", referencedColumnName = "id")
+    private Warehouse warehouse;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tax_format", columnDefinition = "enum('PERCENT','AMOUNT')")
+    private TaxFormat taxFormat;
+
+    @Column(name = "tax")
+    private BigDecimal tax;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "discount_format", columnDefinition = "enum('PERCENT','AMOUNT')")
+    private DiscountFormat discountFormat;
+
+    @Column(name = "discount")
+    private BigDecimal discount;
+
+    @Column(name = "amount")
+    private BigDecimal amount;
+
+}
