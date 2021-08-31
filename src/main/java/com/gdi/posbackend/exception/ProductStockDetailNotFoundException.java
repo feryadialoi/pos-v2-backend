f0cdd61595084f9ae963ae3054c20e5c1948a482
Feryadi Aloi <feryadialoi@gmail.com>
2021-08-31 13:32:18 +0700

    create custom exception of ProductStockDetailNotFoundException

---
+++ src/main/java/com/gdi/posbackend/exception/ProductStockDetailNotFoundException.java
@@ -0,0 +1,13 @@
+package com.gdi.posbackend.exception;
+
+/**
+ * @author Feryadialoi
+ * @date 8/30/2021 1:35 AM
+ */
+public class ProductStockDetailNotFoundException extends RuntimeException {
+    public ProductStockDetailNotFoundException() {
+    }
+
+    public ProductStockDetailNotFoundException(String message) {
+    }
+}
