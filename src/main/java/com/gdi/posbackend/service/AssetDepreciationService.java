8414cbd33dc647b5641cbf084f82c74d751fb1f3
Feryadi Aloi <feryadialoi@gmail.com>
2021-08-31 13:36:50 +0700

    update service AssetDepreciationService

---
+++ src/main/java/com/gdi/posbackend/service/AssetDepreciationService.java
@@ -0,0 +1,8 @@
+package com.gdi.posbackend.service;
+
+/**
+ * @author Feryadialoi
+ * @date 8/20/2021 3:03 AM
+ */
+public interface AssetDepreciationService {
+}
---
+++ src/main/java/com/gdi/posbackend/service/impl/AssetDepreciationServiceImpl.java
@@ -0,0 +1,17 @@
+package com.gdi.posbackend.service.impl;
+
+import com.gdi.posbackend.service.AssetDepreciationService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Feryadialoi
+ * @date 8/20/2021 3:10 AM
+ */
+@Service
+@AllArgsConstructor
+public class AssetDepreciationServiceImpl implements AssetDepreciationService {
+
+
+
+}
