1d183b94e4d5be4167e32500e8f68a04fbe709f4
Feryadi Aloi <feryadialoi@gmail.com>
2021-08-31 13:26:19 +0700

    create new entity of Sale and SaleDetail

---
+++ src/main/java/com/gdi/posbackend/entity/Sale.java
@@ -0,0 +1,8 @@
+package com.gdi.posbackend.entity;
+
+/**
+ * @author Feryadialoi
+ * @date 8/29/2021 3:46 AM
+ */
+public class Sale {
+}
---
+++ src/main/java/com/gdi/posbackend/entity/SaleDetail.java
@@ -0,0 +1,8 @@
+package com.gdi.posbackend.entity;
+
+/**
+ * @author Feryadialoi
+ * @date 8/31/2021 1:26 PM
+ */
+public class SaleDetail {
+}
