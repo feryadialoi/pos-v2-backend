676fa3635412b4ce64d7f209f4fc4bea6096a306
Feryadi Aloi <feryadialoi@gmail.com>
2021-08-31 13:42:35 +0700

    add method updateProductStockByPurchase to ProductStockService

--- src/main/java/com/gdi/posbackend/service/ProductStockService.java
+++ src/main/java/com/gdi/posbackend/service/ProductStockService.java
@@ -1,5 +1,6 @@
 package com.gdi.posbackend.service;
 
+import com.gdi.posbackend.entity.Purchase;
 import com.gdi.posbackend.model.criteria.ProductStockCriteria;
 import com.gdi.posbackend.model.request.UpdateProductStockRequest;
 import org.springframework.data.domain.Page;
@@ -15,4 +16,6 @@ public interface ProductStockService {
     Object getProductStock(String productStockId);
 
     Object updateProductStock(String productStockId, UpdateProductStockRequest updateProductStockRequest);
+
+    void updateProductStockByPurchase(Purchase purchase);
 }
--- src/main/java/com/gdi/posbackend/service/impl/ProductStockServiceImpl.java
+++ src/main/java/com/gdi/posbackend/service/impl/ProductStockServiceImpl.java
@@ -1,24 +1,46 @@
 package com.gdi.posbackend.service.impl;
 
-import com.gdi.posbackend.entity.Product;
+import com.gdi.posbackend.command.productstock.UpdateProductStockByPurchaseCommand;
+import com.gdi.posbackend.entity.*;
+import com.gdi.posbackend.entity.enums.CogsMethod;
+import com.gdi.posbackend.exception.ProductStockDetailNotFoundException;
+import com.gdi.posbackend.model.commandparam.UpdateProductStockByPurchaseCommandParam;
 import com.gdi.posbackend.model.criteria.ProductStockCriteria;
 import com.gdi.posbackend.model.request.UpdateProductStockRequest;
 import com.gdi.posbackend.repository.ProductRepository;
+import com.gdi.posbackend.repository.ProductStockDetailRepository;
+import com.gdi.posbackend.repository.ProductStockMutationRepository;
+import com.gdi.posbackend.repository.ProductStockRepository;
+import com.gdi.posbackend.service.CogsService;
 import com.gdi.posbackend.service.ProductStockService;
+import com.gdi.posbackend.service.RunningNumberService;
+import com.gdi.posbackend.service.ServiceExecutor;
+import com.gdi.posbackend.util.CompanySettingValueUtil;
+import com.gdi.posbackend.util.RunningNumberCodeUtil;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
 
 /**
  * @author Feryadialoi
  * @date 8/6/2021 2:03 AM
  */
 @Service
+@Transactional
 @AllArgsConstructor
 public class ProductStockServiceImpl implements ProductStockService {
 
+    // ** repository
     private final ProductRepository productRepository;
+    private final ProductStockDetailRepository productStockDetailRepository;
+
+    // ** service
+    private final CogsService cogsService;
+    private final ServiceExecutor serviceExecutor;
 
 
     @Override
@@ -39,4 +61,36 @@ public class ProductStockServiceImpl implements ProductStockService {
     public Object updateProductStock(String productStockId, UpdateProductStockRequest updateProductStockRequest) {
         return null;
     }
+
+    @Override
+    public void updateProductStockByPurchase(Purchase purchase) {
+        serviceExecutor.execute(UpdateProductStockByPurchaseCommand.class, new UpdateProductStockByPurchaseCommandParam(purchase));
+    }
+
+    private ProductStockDetail findProductStockDetail(ProductStock productStock) {
+
+        CogsMethod cogsMethod = cogsService.getCogsMethod();
+
+        String notFoundMessage = "product stock detail with product stock of id "
+                + productStock.getId()
+                + " not found";
+
+        switch (cogsMethod) {
+            case FIFO:
+                return productStockDetailRepository
+                        .findProductStockDetailByProductStockAndQuantityGreaterThanOrderByCreatedDateAsc(productStock, BigDecimal.ZERO)
+                        .orElseThrow(() -> new ProductStockDetailNotFoundException(notFoundMessage));
+            case LIFO:
+                return productStockDetailRepository
+                        .findProductStockDetailByProductStockAndQuantityGreaterThanOrderByCreatedDateDesc(productStock, BigDecimal.ZERO)
+                        .orElseThrow(() -> new ProductStockDetailNotFoundException(notFoundMessage));
+            case WEIGHTED_AVERAGE:
+                return productStockDetailRepository
+                        .findProductStockDetailByProductStockAndQuantityGreaterThanOrderByCreatedDateAsc(productStock, BigDecimal.ZERO)
+                        .orElseThrow(() -> new ProductStockDetailNotFoundException(notFoundMessage));
+            default:
+                throw new RuntimeException("not supported type");
+        }
+    }
+
 }
