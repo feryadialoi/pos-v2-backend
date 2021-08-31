06aa3561f03f69d629170dbb50f51b58bb4383bb
Feryadi Aloi <feryadialoi@gmail.com>
2021-08-31 13:53:47 +0700

    create controller of SaleController

---
+++ src/main/java/com/gdi/posbackend/controller/v1/SaleController.java
@@ -0,0 +1,43 @@
+package com.gdi.posbackend.controller.v1;
+
+import com.gdi.posbackend.controller.BaseController;
+import com.gdi.posbackend.model.criteria.SaleCriteria;
+import com.gdi.posbackend.model.request.CreateSaleRequest;
+import com.gdi.posbackend.model.response.ApiResponse;
+import com.gdi.posbackend.model.response.DetailedSaleResponse;
+import com.gdi.posbackend.model.response.SaleResponse;
+import com.gdi.posbackend.service.SaleService;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+/**
+ * @author Feryadialoi
+ * @date 8/18/2021 11:42 PM
+ */
+@RestController
+@RequestMapping("/api/v1/sales")
+@AllArgsConstructor
+public class SaleController extends BaseController {
+
+    private final SaleService saleService;
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<Page<SaleResponse>>> getSales(SaleCriteria saleCriteria, Pageable pageable) {
+        return response("get sales success", saleService.getSales(saleCriteria, pageable));
+    }
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<DetailedSaleResponse>> createSale(@Valid @RequestBody CreateSaleRequest createSaleRequest) {
+        return response("create sale success", saleService.createSale(createSaleRequest));
+    }
+
+    @GetMapping("/{saleId}")
+    public ResponseEntity<ApiResponse<DetailedSaleResponse>> getSale(@PathVariable(name = "saleId") String saleId) {
+        return response("get sale success", saleService.getSale(saleId));
+    }
+}
