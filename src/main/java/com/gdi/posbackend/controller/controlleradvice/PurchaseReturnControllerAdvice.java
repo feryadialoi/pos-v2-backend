c192b1809840e6094627d19c2f240a21af3322cc
Feryadi Aloi <feryadialoi@gmail.com>
2021-09-07 03:22:13 +0700

    [core] create controller advice PurchaseReturnControllerAdvice

---
+++ src/main/java/com/gdi/posbackend/controller/controlleradvice/PurchaseReturnControllerAdvice.java
@@ -0,0 +1,21 @@
+package com.gdi.posbackend.controller.controlleradvice;
+
+import com.gdi.posbackend.controller.BaseControllerAdvice;
+import com.gdi.posbackend.exception.PurchaseNotFoundException;
+import com.gdi.posbackend.exception.PurchaseReturnNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * @author Feryadialoi
+ * @date 8/30/2021 11:18 AM
+ */
+@RestControllerAdvice
+public class PurchaseReturnControllerAdvice extends BaseControllerAdvice {
+
+    @ExceptionHandler(PurchaseReturnNotFoundException.class)
+    public Object purchaseReturnNotFound(PurchaseReturnNotFoundException purchaseReturnNotFoundException) {
+        return response("not found", purchaseReturnNotFoundException.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
