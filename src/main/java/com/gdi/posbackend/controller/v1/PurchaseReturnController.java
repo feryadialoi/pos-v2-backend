7c4182661d7fd3e9fa7fd581caacba3ef43517ac
Feryadi Aloi <feryadialoi@gmail.com>
2021-09-07 03:08:15 +0700

    [core] create PurchaseReturnController

---
+++ src/main/java/com/gdi/posbackend/controller/v1/PurchaseReturnController.java
@@ -0,0 +1,18 @@
+package com.gdi.posbackend.controller.v1;
+
+import com.gdi.posbackend.controller.BaseController;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Feryadialoi
+ * @date 9/5/2021 2:03 AM
+ */
+@RestController
+@RequestMapping("/api/v1/purchase-returns")
+@AllArgsConstructor
+public class PurchaseReturnController extends BaseController {
+
+
+}
