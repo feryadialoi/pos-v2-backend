2c5b1b8d473620ae80b1ac079b7cf9b37a1e6ecf
Feryadi Aloi <feryadialoi@gmail.com>
2021-09-07 03:33:21 +0700

    add new setting name temporary to SettingNameOfCompanySetting

--- src/main/java/com/gdi/posbackend/constant/SettingNameOfCompanySetting.java
+++ src/main/java/com/gdi/posbackend/constant/SettingNameOfCompanySetting.java
@@ -13,33 +13,29 @@ public enum SettingNameOfCompanySetting {
     TAX_RATED,
 
     COA_PURCHASE,
+    COA_PURCHASE_RETURN,
+    COA_PURCHASE_DISCOUNT,
+    COA_PURCHASE_TAX,
+
     COA_SALE,
+    COA_SALE_RETURN,
+    COA_SALE_DISCOUNT,
+    COA_SALE_TAX,
+
     COA_INCOME_SUMMARY,
     COA_RETAINED_EARNING,
+
     COA_PAYABLE,
     COA_RECEIVABLE,
-    COA_TAX_IN,
-    COA_TAX_OUT,
+
+    COA_TAX_IN, // ** for purchase
+    COA_TAX_OUT, // ** for sale
+
     COA_DISCOUNT_IN,
     COA_DISCOUNT_OUT,
 
-    COGS_METHOD,
+    COA_OTHER_FEE,
 
-//    public static final String UNIT = "UNIT";
-//    public static final String ACCOUNTING = "ACCOUNTING";
-//    public static final String COGS = "COGS";
-//    public static final String IS_TAXED = "IS_TAXED";
-//    public static final String TAX_RATED = "TAX_RATED";
-//
-//    public static final String COA_PURCHASE = "COA_PURCHASE";
-//    public static final String COA_SALE = "COA_SALE";
-//    public static final String COA_INCOME_SUMMARY = "COA_INCOME_SUMMARY";
-//    public static final String COA_RETAINED_EARNING = "COA_RETAINED_EARNING";
-//    public static final String COA_PAYABLE = "COA_PAYABLE";
-//    public static final String COA_RECEIVABLE = "COA_RECEIVABLE";
-//    public static final String COA_TAX_IN = "COA_TAX_IN";
-//    public static final String COA_TAX_OUT = "COA_TAX_OUT";
-//    public static final String COA_DISCOUNT_IN = "COA_DISCOUNT_IN";
-//    public static final String COA_DISCOUNT_OUT = "COA_DISCOUNT_OUT";
+    COGS_METHOD,
 
 }
