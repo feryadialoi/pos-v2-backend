cad9cfe4c1124de80de67cff5c7659f98cedd87f
Feryadi Aloi <feryadialoi@gmail.com>
2021-09-07 02:51:28 +0700

    [migration] create table customers

---
+++ src/main/resources/db/migration/V20210902135650__create_customers_table.sql
@@ -0,0 +1,17 @@
+create table customers
+(
+    id                 varchar(36),
+    company_id         varchar(36),
+
+
+    name               varchar(255),
+    address            varchar(255),
+
+
+    created_date       timestamp(3),
+    last_modified_date timestamp(3),
+    deleted_date       timestamp(3),
+    created_by         varchar(36),
+    last_modified_by   varchar(36),
+    primary key (id)
+) engine = InnoDB;
