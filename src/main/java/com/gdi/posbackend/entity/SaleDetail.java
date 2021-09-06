eb238efc2da20a1857eb768fc08ca8f8069eb4f2
Feryadi Aloi <feryadialoi@gmail.com>
2021-09-07 03:29:21 +0700

    [core] update SaleDetail entity add required column

--- src/main/java/com/gdi/posbackend/entity/SaleDetail.java
+++ src/main/java/com/gdi/posbackend/entity/SaleDetail.java
@@ -1,8 +1,65 @@
 package com.gdi.posbackend.entity;
 
+import com.gdi.posbackend.entity.enums.DiscountFormat;
+import com.gdi.posbackend.entity.enums.TaxFormat;
+import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
 /**
  * @author Feryadialoi
  * @date 8/31/2021 1:26 PM
  */
-public class SaleDetail {
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "sale_details")
+@SQLDelete(sql = "UPDATE sale_details SET deleted_date = NOW() WHERE id = ?")
+@Where(clause = "deleted_date is null")
+public class SaleDetail extends BaseEntity {
+
+    @ManyToOne
+    @JoinColumn(name = "sale_id", referencedColumnName = "id")
+    private Sale sale;
+
+    @OneToOne
+    @JoinColumn(name = "product_id", referencedColumnName = "id")
+    private Product product;
+
+    @Column(name = "price")
+    private BigDecimal price;
+
+    @Column(name = "quantity")
+    private BigDecimal quantity;
+
+    @OneToOne
+    @JoinColumn(name = "unit_id", referencedColumnName = "id")
+    private Unit unit;
+
+    @OneToOne
+    @JoinColumn(name = "warehouse_id", referencedColumnName = "id")
+    private Warehouse warehouse;
+
+    @Column(name = "tax")
+    private BigDecimal tax;
+
+    @Column(name = "tax_format", columnDefinition = "enum('PERCENT','AMOUNT')")
+    private TaxFormat taxFormat;
+
+    @Column(name = "discount")
+    private BigDecimal discount;
+
+    @Column(name = "discount_format", columnDefinition = "enum('PERCENT','AMOUNT')")
+    private DiscountFormat discountFormat;
+
+    @Column(name = "total")
+    private BigDecimal total;
+
 }
