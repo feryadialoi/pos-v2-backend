86430b6d61ab4f9cae8ae30e7a2b633de8546023
Feryadi Aloi <feryadialoi@gmail.com>
2021-09-07 03:03:01 +0700

    [core] create SaleOrderStatus and SaleStatus

---
+++ src/main/java/com/gdi/posbackend/entity/enums/SaleOrderStatus.java
@@ -0,0 +1,9 @@
+package com.gdi.posbackend.entity.enums;
+
+/**
+ * @author Feryadialoi
+ * @date 9/2/2021 11:06 PM
+ */
+public enum SaleOrderStatus {
+    DRAFT, SENT, DECLINED, ACCEPTED
+}
---
+++ src/main/java/com/gdi/posbackend/entity/enums/SaleStatus.java
@@ -0,0 +1,9 @@
+package com.gdi.posbackend.entity.enums;
+
+/**
+ * @author Feryadialoi
+ * @date 9/3/2021 1:20 AM
+ */
+public enum SaleStatus {
+    UNPAID, PARTIAL_PAID, PAID, VOID
+}
