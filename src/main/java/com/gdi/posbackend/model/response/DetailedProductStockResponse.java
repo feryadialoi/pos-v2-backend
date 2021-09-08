4565156638a26c6b4477152df229523595a5ee14
Feryadi Aloi <feryadialoi@gmail.com>
2021-09-08 09:41:40 +0700

    [core] create model DetailedProductStockResponse and ProductStockDetailResponse

---
+++ src/main/java/com/gdi/posbackend/model/response/DetailedProductStockResponse.java
@@ -0,0 +1,25 @@
+package com.gdi.posbackend.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ * @author Feryadialoi
+ * @date 9/7/2021 3:05 PM
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DetailedProductStockResponse {
+    private String id;
+    private ProductResponse product;
+    private UnitResponse unit;
+    private BigDecimal stock;
+    private List<ProductStockDetailResponse> productStockDetails;
+}
---
+++ src/main/java/com/gdi/posbackend/model/response/ProductStockDetailResponse.java
@@ -0,0 +1,22 @@
+package com.gdi.posbackend.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+/**
+ * @author Feryadialoi
+ * @date 9/7/2021 3:06 PM
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductStockDetailResponse {
+    private String id;
+    private BigDecimal quantity;
+    private String batch;
+}
