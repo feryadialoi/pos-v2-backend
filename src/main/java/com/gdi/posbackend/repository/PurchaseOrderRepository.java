ced1aa661f2cd67f69a5be32e4aecde5eaa5d140
Feryadi Aloi <feryadialoi@gmail.com>
2021-09-11 12:40:55 +0700

    [core] add method findPurchaseOrderByIdOrThrowNotFound to PurchaseOrderRepository

--- src/main/java/com/gdi/posbackend/repository/PurchaseOrderRepository.java
+++ src/main/java/com/gdi/posbackend/repository/PurchaseOrderRepository.java
@@ -1,6 +1,7 @@
 package com.gdi.posbackend.repository;
 
 import com.gdi.posbackend.entity.PurchaseOrder;
+import com.gdi.posbackend.exception.PurchaseOrderNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
@@ -9,4 +10,7 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
  * @date 8/23/2021 10:24 AM
  */
 public interface PurchaseOrderRepository extends JpaRepository<PurchaseOrder, String>, JpaSpecificationExecutor<PurchaseOrder> {
+    default PurchaseOrder findPurchaseOrderByIdOrThrowNotFound(String purchaseOrderId) {
+        return findById(purchaseOrderId).orElseThrow(() -> new PurchaseOrderNotFoundException("purchase order with id " + purchaseOrderId + " not found"));
+    }
 }
