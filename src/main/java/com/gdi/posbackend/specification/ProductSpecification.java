be7d3610ad21c1bed41a437be351ecefbc0450f3
Feryadi Aloi <feryadialoi@gmail.com>
2021-09-11 12:26:04 +0700

    [core] update ProductSpecification set nameIsLike, codeIsLike, categoryNamIsLike to not case sensitive, add brandNameIsLike

--- src/main/java/com/gdi/posbackend/specification/ProductSpecification.java
+++ src/main/java/com/gdi/posbackend/specification/ProductSpecification.java
@@ -8,18 +8,33 @@ import org.springframework.data.jpa.domain.Specification;
  * @date 8/5/2021 10:39 AM
  */
 public class ProductSpecification {
+
     public static Specification<Product> nameIsLike(String name) {
-        return ((root, criteriaQuery, criteriaBuilder) ->
-                criteriaBuilder.like(root.get("name"), "%" + name + "%"));
+        return (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.like(
+                root.get("name"),
+                "%" + name + "%"
+        );
     }
 
     public static Specification<Product> codeIsLike(String code) {
-        return ((root, criteriaQuery, criteriaBuilder) ->
-                criteriaBuilder.like(root.get("code"), "%" + code + "%"));
+        return (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.like(
+                root.get("code"),
+                "%" + code + "%"
+        );
     }
 
     public static Specification<Product> categoryNameIsLike(String categoryName) {
-        return ((root, criteriaQuery, criteriaBuilder) ->
-                criteriaBuilder.like(root.get("category").get("name"), "%" + categoryName + "%"));
+        return (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.like(
+                root.get("category").get("name"),
+                "%" + categoryName + "%"
+        );
     }
+
+    public static Specification<Product> brandNameIsLike(String brandName) {
+        return (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.like(
+                root.get("brand").get("name"),
+                "%" + brandName + "%"
+        );
+    }
+
 }
