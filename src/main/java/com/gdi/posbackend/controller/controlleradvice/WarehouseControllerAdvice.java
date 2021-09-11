29b88c5fd1d6f657c933936cdb6596828630c53d
Feryadi Aloi <feryadialoi@gmail.com>
2021-09-11 12:28:36 +0700

    [core] create WarehouseControllerAdvice

---
+++ src/main/java/com/gdi/posbackend/controller/controlleradvice/WarehouseControllerAdvice.java
@@ -0,0 +1,28 @@
+package com.gdi.posbackend.controller.controlleradvice;
+
+import com.gdi.posbackend.controller.BaseControllerAdvice;
+import com.gdi.posbackend.exception.WarehouseNotFoundException;
+import com.gdi.posbackend.exception.WarehouseUsedDeleteNotAllowed;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * @author Feryadialoi
+ * @date 9/11/2021 10:54 AM
+ */
+@RestControllerAdvice
+public class WarehouseControllerAdvice extends BaseControllerAdvice {
+
+    @ExceptionHandler(WarehouseNotFoundException.class)
+    public Object warehouseNotFound(WarehouseNotFoundException warehouseNotFoundException) {
+        return response("not found", warehouseNotFoundException.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(WarehouseUsedDeleteNotAllowed.class)
+    public Object warehouseUsedDeleteNotAllowed(WarehouseUsedDeleteNotAllowed warehouseUsedDeleteNotAllowed) {
+        return response("bad request", warehouseUsedDeleteNotAllowed.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+
+}
