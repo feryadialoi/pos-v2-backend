c24bd61baffb4465aaf3ab4c7a81d2747156e3c5
Feryadi Aloi <feryadialoi@gmail.com>
2021-09-11 12:38:18 +0700

    [core] add validation rule to CreateSaleOrderRequest

--- src/main/java/com/gdi/posbackend/model/request/CreateSaleOrderRequest.java
+++ src/main/java/com/gdi/posbackend/model/request/CreateSaleOrderRequest.java
@@ -2,8 +2,14 @@ package com.gdi.posbackend.model.request;
 
 import com.gdi.posbackend.entity.enums.PaymentType;
 import com.gdi.posbackend.entity.enums.SaleOrderStatus;
+import com.gdi.posbackend.validation.RegexValidationRule;
+import com.gdi.posbackend.validation.constraint.ProductOfCreatePurchaseOrderRequestConstraint;
+import com.gdi.posbackend.validation.constraint.ProductOfCreateSaleOrderRequestConstraint;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -13,18 +19,43 @@ import java.util.List;
  */
 @Data
 public class CreateSaleOrderRequest {
+    @NotNull
+    @NotBlank
     private String customerId;
+
+    @NotNull
+    @NotBlank
     private String salesmanId;
+
+    @Pattern(regexp = RegexValidationRule.dateStringFormat, message = RegexValidationRule.dateStringFormatMessage)
+    @NotNull
+    @NotBlank
     private String entryDate;
+
+    @Pattern(regexp = RegexValidationRule.dateStringFormat, message = RegexValidationRule.dateStringFormatMessage)
     private String dueDate;
+
     private Integer term;
+
     private String reference;
+
     private String note;
+
+    @NotNull
     private PaymentType paymentType;
+
     private BigDecimal shippingFee;
+
     private String shippingFeeDescription;
+
     private BigDecimal otherFee;
+
     private String otherFeeDescription;
+
+    @NotNull
+    @ProductOfCreateSaleOrderRequestConstraint
     private List<ProductOfCreateSaleOrderRequest> products;
+
     private SaleOrderStatus status;
+
 }
