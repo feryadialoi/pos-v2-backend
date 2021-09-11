56ac117f6f3becaf0319aee5cb28832ec1b40146
Feryadi Aloi <feryadialoi@gmail.com>
2021-09-11 12:26:45 +0700

    [core] update WarehouseSpecification set nameIsLike, addressIsLike to not case sensitive

--- src/main/java/com/gdi/posbackend/specification/WarehouseSpecification.java
+++ src/main/java/com/gdi/posbackend/specification/WarehouseSpecification.java
@@ -3,16 +3,24 @@ package com.gdi.posbackend.specification;
 import com.gdi.posbackend.entity.Warehouse;
 import org.springframework.data.jpa.domain.Specification;
 
+import java.util.Locale;
+
 /**
  * @author Feryadialoi
  * @date 8/5/2021 3:36 AM
  */
 public class WarehouseSpecification {
     public static Specification<Warehouse> nameIsLike(String name) {
-        return (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.like(root.get("name"), "%" + name + "%");
+        return (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.like(
+                criteriaBuilder.upper(root.get("name")),
+                "%" + name.toUpperCase() + "%"
+        );
     }
 
     public static Specification<Warehouse> addressIsLike(String address) {
-        return ((root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.like(root.get("address"), "%" + address + "%"));
+        return (root, criteriaQuery, criteriaBuilder) -> criteriaBuilder.like(
+                criteriaBuilder.upper(root.get("address")),
+                "%" + address.toUpperCase() + "%"
+        );
     }
 }
