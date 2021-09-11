b885f9ebc42a1e4e4a9ebc9114b8f6fd034a2100
Feryadi Aloi <feryadialoi@gmail.com>
2021-09-11 12:40:38 +0700

    [core] add method findByIdOrThrowNotFound to ProductRepository

--- src/main/java/com/gdi/posbackend/repository/ProductRepository.java
+++ src/main/java/com/gdi/posbackend/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.gdi.posbackend.repository;
 
 import com.gdi.posbackend.entity.Product;
+import com.gdi.posbackend.exception.ProductNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
@@ -11,5 +12,9 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 public interface ProductRepository extends JpaRepository<Product, String>, JpaSpecificationExecutor<Product> {
 
     boolean existsByIdempotentKey(String idempotentKey);
+
+    default Product findByIdOrThrowNotFound(String productId) {
+        return findById(productId).orElseThrow(() -> new ProductNotFoundException("product with id " + productId + " not found"));
+    }
 
 }
