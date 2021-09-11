9ba93e58dbe4e87b215cd7d62dda66d53af77364
Feryadi Aloi <feryadialoi@gmail.com>
2021-09-11 12:37:44 +0700

    [core] add pattern regex validation to CreatePurchaseOrderRequest date string property

--- src/main/java/com/gdi/posbackend/model/request/CreatePurchaseOrderRequest.java
+++ src/main/java/com/gdi/posbackend/model/request/CreatePurchaseOrderRequest.java
@@ -4,11 +4,13 @@ import com.gdi.posbackend.entity.enums.DiscountFormat;
 import com.gdi.posbackend.entity.enums.PaymentType;
 import com.gdi.posbackend.entity.enums.PurchaseOrderStatus;
 import com.gdi.posbackend.entity.enums.TaxFormat;
+import com.gdi.posbackend.validation.RegexValidationRule;
 import com.gdi.posbackend.validation.constraint.ProductOfCreatePurchaseOrderRequestConstraint;
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -23,12 +25,12 @@ public class CreatePurchaseOrderRequest {
     @NotBlank
     private String supplierId;
 
+    @Pattern(regexp = RegexValidationRule.dateStringFormat, message = RegexValidationRule.dateStringFormatMessage)
     @NotNull
     @NotBlank
     private String entryDate;
 
-    @NotNull
-    @NotBlank
+    @Pattern(regexp = RegexValidationRule.dateStringFormat, message = RegexValidationRule.dateStringFormatMessage)
     private String dueDate;
 
     private Integer term;
