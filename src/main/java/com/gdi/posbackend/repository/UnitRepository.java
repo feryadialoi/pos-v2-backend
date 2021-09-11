d8dc371a5736bc513dd884ec4140a605b83cafcf
Feryadi Aloi <feryadialoi@gmail.com>
2021-09-11 12:41:17 +0700

    [core] add method findByIdOrThrowNotFound and productCountByBrandId to UnitRepository

--- src/main/java/com/gdi/posbackend/repository/UnitRepository.java
+++ src/main/java/com/gdi/posbackend/repository/UnitRepository.java
@@ -1,9 +1,11 @@
 package com.gdi.posbackend.repository;
 
 import com.gdi.posbackend.entity.Unit;
+import com.gdi.posbackend.exception.UnitNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,4 +21,10 @@ public interface UnitRepository extends JpaRepository<Unit, String>, JpaSpecific
 
     long countByNameInIgnoreCase(List<String> names);
 
+    default Unit findByIdOrThrowNotFound(String unitId) {
+        return findById(unitId).orElseThrow(() -> new UnitNotFoundException("unit with id " + unitId + " not found"));
+    }
+
+    @Query(value = "SELECT COUNT(*) FROM products_units WHERE unit_id = :brandId", nativeQuery = true)
+    long productCountByBrandId(@Param("brandId") String brandId);
 }
