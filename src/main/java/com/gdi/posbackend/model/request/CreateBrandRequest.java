6cbebb59ac65c9fb339a5d2d49e606dabee0406d
Feryadi Aloi <feryadialoi@gmail.com>
2021-09-12 21:04:42 +0700

    [core] add @NotNull, @NotBlank to CreateBrandRequest and CreateCategoryRequest

--- src/main/java/com/gdi/posbackend/model/request/CreateBrandRequest.java
+++ src/main/java/com/gdi/posbackend/model/request/CreateBrandRequest.java
@@ -2,11 +2,16 @@ package com.gdi.posbackend.model.request;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 /**
  * @author Feryadialoi
  * @date 8/13/2021 2:11 PM
  */
 @Data
 public class CreateBrandRequest {
+    @NotNull
+    @NotBlank
     private String name;
 }
--- src/main/java/com/gdi/posbackend/model/request/CreateCategoryRequest.java
+++ src/main/java/com/gdi/posbackend/model/request/CreateCategoryRequest.java
@@ -2,11 +2,16 @@ package com.gdi.posbackend.model.request;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 /**
  * @author Feryadialoi
  * @date 8/5/2021 5:24 AM
  */
 @Data
 public class CreateCategoryRequest {
+    @NotNull
+    @NotBlank
     private String name;
 }
