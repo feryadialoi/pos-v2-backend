5fa801bc77837d9dda1db259d4f8150de143bc4b
Feryadi Aloi <feryadialoi@gmail.com>
2021-09-26 23:27:26 +0700

    [core] add method countBySupplier and existsByPurchaseOrder in PurchaseRepository

--- src/main/java/com/gdi/posbackend/repository/PurchaseRepository.java
+++ src/main/java/com/gdi/posbackend/repository/PurchaseRepository.java
@@ -1,6 +1,8 @@
 package com.gdi.posbackend.repository;
 
 import com.gdi.posbackend.entity.Purchase;
+import com.gdi.posbackend.entity.PurchaseOrder;
+import com.gdi.posbackend.entity.Supplier;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
@@ -9,4 +11,9 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
  * @date 8/25/2021 1:50 AM
  */
 public interface PurchaseRepository extends JpaRepository<Purchase, String>, JpaSpecificationExecutor<Purchase> {
+
+    long countBySupplier(Supplier supplier);
+
+    boolean existsByPurchaseOrder(PurchaseOrder purchaseOrder);
+
 }
