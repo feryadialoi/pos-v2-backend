bfb7a160f7c08f6726747db9583ffe4b247da513
Feryadi Aloi <feryadialoi@gmail.com>
2021-09-26 22:59:58 +0700

    [core] add DateConfig date format

---
+++ src/main/java/com/gdi/posbackend/config/DateConfig.java
@@ -0,0 +1,30 @@
+package com.gdi.posbackend.config;
+
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.format.DateTimeFormatter;
+
+/**
+ * @author Feryadialoi
+ * @date 9/13/2021 2:07 PM
+ */
+@Configuration
+public class DateConfig {
+
+    public static final String dateFormat = "yyyy-MM-dd";
+    public static final String dateTimeFormat = "yyyy-MM-dd HH:mm:ss";
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
+        return builder -> {
+            builder.simpleDateFormat(dateTimeFormat);
+            builder.serializers(new LocalDateSerializer(DateTimeFormatter.ofPattern(dateFormat)));
+            builder.serializers(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(dateTimeFormat)));
+        };
+    }
+
+}
