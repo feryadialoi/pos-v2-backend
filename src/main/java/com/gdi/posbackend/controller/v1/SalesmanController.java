77952ed77c7038f8a893b7a8ccd981dcd55cbd7d
Feryadi Aloi <feryadialoi@gmail.com>
2021-10-04 10:55:28 +0700

    [core] add logging to SalesmanController

--- src/main/java/com/gdi/posbackend/controller/v1/SalesmanController.java
+++ src/main/java/com/gdi/posbackend/controller/v1/SalesmanController.java
@@ -8,6 +8,7 @@ import com.gdi.posbackend.model.response.ApiResponse;
 import com.gdi.posbackend.model.response.SalesmanResponse;
 import com.gdi.posbackend.service.SalesmanService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +21,7 @@ import java.util.List;
  * @author Feryadialoi
  * @date 9/14/2021 4:04 PM
  */
+@Slf4j
 @RestController
 @RequestMapping("/api/v1/salesmen")
 @AllArgsConstructor
@@ -29,31 +31,42 @@ public class SalesmanController extends BaseController {
 
     @GetMapping
     public ResponseEntity<ApiResponse<Page<SalesmanResponse>>> getSalesmen(SalesmanCriteria salesmanCriteria, Pageable pageable) {
+        log.info("getSalesman with salesmanCriteria : {}, and pageable : {}", salesmanCriteria, pageable);
+
         return response("get salesmen success", salesmanService.getSalesmen(salesmanCriteria, pageable));
     }
 
     @PostMapping
     public ResponseEntity<ApiResponse<SalesmanResponse>> createSalesman(@Valid @RequestBody CreateSalesmanRequest createSalesmanRequest) {
+        log.info("createSalesman with createSalesmanRequest : {}", createSalesmanRequest);
+
         return response("create salesman success", salesmanService.createSalesman(createSalesmanRequest));
     }
 
     @GetMapping("/list")
     public ResponseEntity<ApiResponse<List<SalesmanResponse>>> getListSalesman() {
+        log.info("getListSalesman");
+
         return response("get list salesman success", salesmanService.getListSalesman());
     }
 
     @GetMapping("/{salesmanId}")
     public ResponseEntity<ApiResponse<SalesmanResponse>> getSalesman(@PathVariable(name = "salesmanId") String salesmanId) {
+        log.info("getSalesman with salesmanId : {}", salesmanId);
+
         return response("get salesman success", salesmanService.getSalesman(salesmanId));
     }
 
     @PutMapping("/{salesmanId}")
     public ResponseEntity<ApiResponse<SalesmanResponse>> updateSalesman(@PathVariable(name = "salesmanId") String salesmanId, @Valid @RequestBody UpdateSalesmanRequest updateSalesmanRequest) {
+        log.info("updateSalesman with salesmanId : {}, and updateSalesmanRequest : {}", salesmanId, updateSalesmanRequest);
+
         return response("update salesman success", salesmanService.updateSalesman(salesmanId, updateSalesmanRequest));
     }
 
     @DeleteMapping("/{salesmanId}")
     public ResponseEntity<ApiResponse<String>> deleteSalesman(@PathVariable(name = "salesmanId") String salesmanId) {
+        log.info("deleteSalesman with salemanId : {}", salesmanId);
         return response("delete salesman success", salesmanService.deleteSalesman(salesmanId));
     }
 
